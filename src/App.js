515b473b77b5408a5b4e000014f1b1b62efdd51a
Bo Peng <bopeng.data@gmail.com>
2024-06-21 10:08:51 -0500

    background

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -8,37 +7,28 @@ function App() {
     const script = document.createElement('script');
     script.src = "https://cloud.google.com/ai/gen-app-builder/client?hl=en_US";
     script.async = true;
+    script.onload = () => {
+      // Initialize the widget once the script is loaded
+      const genSearchWidget = document.createElement('gen-search-widget');
+      genSearchWidget.setAttribute('configId', '91c5f2c9-3161-4ec3-8f95-f0cbf69f9c08');
+      genSearchWidget.setAttribute('triggerId', 'searchWidgetTrigger');
+      document.body.appendChild(genSearchWidget);
+    };
     document.body.appendChild(script);
 
     return () => {
       // Clean up the script when the component unmounts
       document.body.removeChild(script);
+      const widget = document.querySelector('gen-search-widget');
+      if (widget) {
+        document.body.removeChild(widget);
+      }
     };
   }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {/* Element that opens the widget on click. It does not have to be an input */}
-        <input placeholder="Search here" id="searchWidgetTrigger" />
-        {/* Search widget element is not visible by default */}
-        <gen-search-widget
-          configId="91c5f2c9-3161-4ec3-8f95-f0cbf69f9c08"
-          triggerId="searchWidgetTrigger">
-        </gen-search-widget>
-      </header>
+      <input placeholder="Search here" id="searchWidgetTrigger" />
     </div>
   );
 }
